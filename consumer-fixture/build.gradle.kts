ef72759b187cd0b456e657b2bea8973f8596eb1a
Peter Isberg <isberg.peter@gmail.com>
2026-04-12 19:07:54 +0200

    bump version to 0.5.2

--- consumer-fixture/build.gradle.kts
+++ consumer-fixture/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "se.deversity.async-test-lib"
-version = "0.5.0"
+version = "0.5.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
@@ -16,7 +16,7 @@ repositories {
     mavenLocal()
 }
 
-val asyncTestVersion = "0.5.0"
+val asyncTestVersion = "0.5.2"
 val junitVersion = "6.0.3"
 
 dependencies {
